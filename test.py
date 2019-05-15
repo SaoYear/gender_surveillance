7c2e447027317f24086ed4406e33cf5dfb12caf1
SaoYear <sao_year@126.com>
2019-05-15 16:50:09 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@
 # filename = []
 # root = []
 # temp = []
-# for r, t, file in os.walk('C:/Users/sn_96/Desktop/毕业设计_基于神经网络/Database/PETA dataset_19000/'):
+# for r, t, file in os.walk('DIR/PETA dataset_19000/'):
 #     filename.append(file)
 #     root.append(r)
 #     temp.append(t)
@@ -13,7 +13,7 @@
 # print(temp)
 
 import cv2 as cv
-img = cv.imread('C:/Users/sn_96/Desktop/FYP_Gender_Processing/Database/pd_dataset/demo/pd_1.jpg')
+img = cv.imread('DIR/pd_1.jpg')
 print(img.shape)
 cv.namedWindow('pic1', cv.WINDOW_AUTOSIZE)
 cv.imshow("pic1", img[5: 5+160, 5: 120])
